9dca4731b297929fe6ba6233dc821a5ef2da9048
omarshaikh99 <omarshaikh@outlook.com>
2021-07-27 14:04:57 +0530

    Update mc_server_status_ping.py

--- mc_server_status_ping.py
+++ mc_server_status_ping.py
@@ -9,3 +9,7 @@ server = MinecraftServer.lookup("servername.org") # Your server name here
 
 status = server.status()
 print("The server has {0} players and replied in {1} ms".format(status.players.online, status.latency))
+
+
+
+
